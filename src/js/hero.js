e00a55c53ce6090ab13906d4c507878d77a556b4
SlavaKholod <kholod.v.job@gmail.com>
2023-04-22 11:07:35 +0300

    add hero styles

---
+++ src/js/hero.js
@@ -0,0 +1,18 @@
+const refs = {
+    cardCont: document.querySelector(".hero__card"),
+    heroBtn: document.querySelector(".start-btn"),
+}
+
+function addCardContent () {
+    const marcup = `<div class="tumb">
+    <img src="#" alt="" />
+  </div>
+  <div class="content">
+    <h2 class="card__header">Space Walking</h2>
+    <div class="content__bio">
+      <img src="#" alt="" />
+      <span>Animakid</span>
+    </div>
+  </div>`;
+  
+}
